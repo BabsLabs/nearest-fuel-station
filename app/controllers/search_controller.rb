6d0b2a6419d65879c5f0230835b1ad4fe0b0ba04
Brian Bower <brian@brianbower.com>
2019-12-20 10:24:23 -0700

    Change HTTP request to use API key header instead of query param

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,21 +3,26 @@ require "net/http"
 
 class SearchController < ApplicationController
 
-  def index
+  def show
+    # I could not figure how to search by location without splitting up the params as seen below!
+    # Also I way prefer NET HTTP to Faraday so I hope thats alright...
     street = params[:location].split(',').first
     postal_code =  params[:location].split(',').last.tr('^0-9', '')
     state = params[:location].split(',').last.tr("0-9", "").gsub!(/\s+/, '')
 
-    url = URI("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&limit=1&api_key=#{ENV['NREL_KEY']}&format=JSON&state=#{state}&street=#{street}&postal code=#{postal_code}")
+    url = URI("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&limit=1&format=JSON&state=#{state}&street=#{street}&postal code=#{postal_code}")
 
     https = Net::HTTP.new(url.host, url.port);
     https.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
+    request["X-Api-Key"] = ENV["NREL_KEY"]
 
     response = https.request(request)
 
     closest_station_results = JSON.parse(response.body, symbolize_names: :true)
+
+    @station = closest_station_results[:fuel_stations][0]
   end
 
 end
