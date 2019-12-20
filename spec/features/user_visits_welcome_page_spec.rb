2354a052cc9068e3ef59e2af1f24d3a0327241a7
Brian Bower <brian@brianbower.com>
2019-12-20 10:46:45 -0700

    Remove commented out user story

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -8,28 +8,3 @@ describe "user can visit the welcome page" do
     expect(page).to have_button("Find Nearest Station")
   end
 end
-
-# We will be using:
-# The NREL alternate fuel stations nearest station API: https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
-# The Google Directions API: https://developers.google.com/maps/documentation/directions/start
-# We will be searching for the nearest electric charging station to Turing.
-# As a user
-# When I visit "/"
-# And I select "Turing" from the start location drop down (Note: Use the existing search form)
-# And I click "Find Nearest Station"
-# Then I should be on page "/search"
-# Then I should see the closest electric fuel station to me.
-# For that station I should see
-# - Name
-# - Address
-# - Fuel Type
-# - Access Times
-# I should also see:
-# - the distance of the nearest station (should be 0.1 miles)
-# - the travel time from Turing to that fuel station (should be 1min)
-# - The HTML direction instructions to get to that fuel station
-#   "Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>"
-# Bonus points!
-# Figure out how to get the directions to display as a string i.e. "Head southeast on 17th St toward Larimer St" instead of as
-# clone this repo: https://github.com/turingschool-examples/nearest-fuel-station
-#
