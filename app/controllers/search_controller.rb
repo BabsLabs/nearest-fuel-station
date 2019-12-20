1fca076fd0327cfbc2a3ada8d3f0bcf3c2ff381e
Brian Bower <brian@brianbower.com>
2019-12-20 11:07:53 -0700

    Add private navigation_search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,12 +5,13 @@ class SearchController < ApplicationController
 
   def show
     nearest_station_search
+    navigation_search
   end
 
   private
 
   def nearest_station_search
-    # I could not figure how to search by location without splitting up the params as seen below!
+    # I could NOT figure how to search by location without splitting up the params as seen below!
     # Also I way prefer NET HTTP to Faraday so I hope thats alright...
     street = params[:location].split(',').first
     postal_code =  params[:location].split(',').last.tr('^0-9', '')
@@ -27,6 +28,28 @@ class SearchController < ApplicationController
     closest_station_results = JSON.parse(https.request(request).body, symbolize_names: :true)
 
     @station = closest_station_results[:fuel_stations][0]
+  end
+
+  def station_address_formattter
+    @station[:street_address] + ' ' + @station[:city] + ' ' + @station[:state] + ' ' + @station[:zip]
+  end
+
+  def navigation_search
+    origin = params[:location]
+    destination = station_address_formattter
+    key = ENV['GOOGLE_DIRECTIONS_API_KEY']
+
+    require "uri"
+    require "net/http"
+
+    url = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&key=#{key}")
+
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+
+    direction_results = JSON.parse(https.request(request).body, symbolize_names: :true)
   end
 
 end
